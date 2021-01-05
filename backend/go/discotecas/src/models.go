e701b699fbde68fe3f510ef847ea0a5e57f980f0
j-maestre <xemaiestacio@gmail.com>
2021-01-05 12:03:12 +0100

    remove events from discotecas and events CRUD OK

--- backend/go/discotecas/src/models.go
+++ backend/go/discotecas/src/models.go
@@ -4,5 +4,4 @@ type Discotecas struct {
 	Id          uint
 	Name        string   `json:"name"`
 	Company     string   `json:"company"`
-	Events      string   `json:"events"`
 }
--- backend/go/events/src/resolvers.go
+++ backend/go/events/src/resolvers.go
@@ -1,15 +1,14 @@
 package events
 
 import (
-	"fmt"
+	
 	"goApp/common"
 
 )
+//"fmt"
 
 //Create event
 func CreateEvent(data interface{}) error{
-	fmt.Println("CREATEEEEEEEE")
-	fmt.Println(data)
 	db:=common.GetDB();
 	err:=db.Create(data).Error
 	return err
@@ -17,12 +16,9 @@ func CreateEvent(data interface{}) error{
 
 //Get all Events
 func GetAllEvents(data interface{}) error{ //Aqui no está pillando bien el name y la discoteca_id
-	fmt.Println("Dentro de getAllEvents");
 	db:=common.GetDB();
-	fmt.Println("Despues de getDB");
 	err:=db.Find(data).Error
-	fmt.Println("Despues del find, err:");
-	fmt.Println(err);
+
 	return err;
 }
 //GET ONE event by ID
@@ -34,18 +30,11 @@ func GetEventById(data, id interface{}) error {
 
 //UPDATE event
 func UpdateEvent(data interface{}) error{
-	fmt.Println("RESOLVER UPDATE");
-	fmt.Println(data)
+
 	db := common.GetDB()
 	err := db.Save(data).Error
 	return err
 }
-
-// func (model *ArticleModel) Update(data interface{}) error {
-// 	db := common.GetDB()
-// 	err := db.Model(model).Update(data).Error
-// 	return err
-// }
 
 //DELETE
 func DeleteEvent(data, id interface{}) error {
