7dd4de42c927ca5f1019055c6d78da4dfb5c3ccf
j-maestre <xemaiestacio@gmail.com>
2021-01-21 11:33:19 +0100

    Xema-> discoteca events and clenaning

--- frontend/angular/src/app/discoteca/component/discoteca-details.component.ts
+++ frontend/angular/src/app/discoteca/component/discoteca-details.component.ts
@@ -4,7 +4,6 @@ import { Observable } from 'rxjs';
 import { DiscotecaPreviewComponent } from './discoteca-preview.component';
 import { ToastrService } from 'ngx-toastr';
 
-
 import {
   Discoteca,
   DiscotecasService
@@ -30,7 +29,6 @@ export class DiscotecaDetailsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-
     //Details
     this.route.data.subscribe((data) => {
       this.discoteca = data.discoteca;
@@ -42,7 +40,6 @@ export class DiscotecaDetailsComponent implements OnInit {
 
   onFavorite() {
     if (this.discoteca.Liked) {//Quitar el favorito
-      console.log("tiene faovrito, hay que quitarselo");
       this.discotecasService.unfavorite(this.discoteca.Id)
         .subscribe(
           data => {
@@ -53,7 +50,6 @@ export class DiscotecaDetailsComponent implements OnInit {
           err => this.toastr.error("Debes iniciar sesion") //this.toastr.error(err.errors.login) 
         );
     } else { //Dar favorito
-      console.log("No tiene favorito, hay que darselo")
       this.discotecasService.favorite(this.discoteca.Id)
         .subscribe(
           data => {
@@ -66,28 +62,4 @@ export class DiscotecaDetailsComponent implements OnInit {
     }
 
   }
-  // onUnFavorite() {
-  //   // this.article.favorited = favorited;
-  //   // this.toastr.success("Hello, I'm the toastr message.")
-
-
-  //   if(this.discoteca.Liked){//Quitar el favorito
-  //     console.log("tiene faovrito, hay que quitarselo");
-  //     //No funciona bien quitar favorito
-  //   }else{//Dar favorito
-  //     console.log("No tiene favorito, hay que darselo")
-  //     this.discotecasService.favorite(this.discoteca.Id)
-  //     .subscribe(
-  //       data => {
-  //         this.toastr.success("Like!");
-  //         // this.router.navigateByUrl('/profile/discotecas');
-  //       },
-  //       err =>this.toastr.error("Debes iniciar sesion") //this.toastr.error(err.errors.login) 
-  //     );
-  //   }
-
-
-  // }
-  //Aqui irá el delete, update, comments, favorited...
-  //Aqui va el favoritos
 }
